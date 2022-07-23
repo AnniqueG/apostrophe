76f2081590b61eb443bcd08c2a9e6bdb7af505ba
anniqueg <anniquemlg@gmail.com>
2022-07-23 13:59:59 -0400

    fix css

--- src/pages/ShoppingCart.js
+++ src/pages/ShoppingCart.js
@@ -1,34 +1,9 @@
 function ShoppingCart() {
     return (
       <div className="App">
-       <header class="section-header">
-          <section class="header-main border-bottom">
-              <div class="container">
-          <div class="row align-items-center">
-
-              <div class="col-lg-4 col-sm-6 col-12">
-                  <div class="widgets-wrap float-md-right">
-                      <div class="widget-header  mr-3">
-                          <a href="#" class="icon icon-sm rounded-circle border"><i class="fa fa-shopping-cart"></i></a>
-                          <span class="badge badge-pill badge-danger notify">0</span>
-                      </div>
-                      <div class="widget-header icontext">
-                          <a href="#" class="icon icon-sm rounded-circle border"><i class="fa fa-user"></i></a>
-                          <div class="text">
-                          </div>
-                      </div>
+        
           
-                  </div> 
-              </div>
-          </div>
-              </div> 
-          </section> 
-          </header> 
-          
-          
-          
-          
-          <section class="section-pagetop bg">
+          <section class="section-pagetop">
           <div class="container">
               <h2 class="title-page">Shopping cart</h2>
           </div> 
