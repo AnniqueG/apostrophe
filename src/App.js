c4a091c8a4c8ca3b73d406fa33307809e9b6bbb3
anniqueg <anniquemlg@gmail.com>
2022-07-24 01:28:20 -0400

    implemented localStorage cart

--- src/App.js
+++ src/App.js
@@ -7,59 +7,11 @@ const App = () => {
   let [cart, setCart] = useState([])
   let localCart = localStorage.getItem("cart");
 
-  const add = (item) => {
-    let cartCopy = [...cart];
-    let {id} = item;
-    
-    let exists = cartCopy.find(cartItem => cartItem.id == id);
-    if(exists){
-        exists.quantity += item.quantity
-    } 
-    else{
-        cartCopy.push(item)
-    }
-
-    setCart(cartCopy)
-
-    let string = JSON.stringify(cartCopy);
-    localStorage.setItem("cart", string)
-  }
-
-  const edit = (id, amount) => {
-    let cartCopy = [...cart]
-    
-    let exists = cartCopy.find(item => item.id == id);
-    
-    if (!exists) return
-    
-    exists.quantity += amount;
-    
-    if (exists.quantity <= 0) {
-      cartCopy = cartCopy.filter(item => item.id != id)
-    }
-    
-
-    setCart(cartCopy);
-    
-    let cartString = JSON.stringify(cartCopy);
-    localStorage.setItem('cart', cartString);
-  }
-
-  const removeItem = (id) => {
-    let cartCopy = [...cart]
-    
-    cartCopy = cartCopy.filter(item => item.id != id);
-    
-    setCart(cartCopy);
-    
-    let cartString = JSON.stringify(cartCopy)
-    localStorage.setItem('cart', cartString)
-  }
-  
   useEffect(() => {
     localCart = JSON.parse(localCart);
     if (localCart) setCart(localCart)
   }, [])
+
    
     return (
     
--- src/ProductList.js
+++ src/ProductList.js
@@ -1,23 +1,49 @@
-import cart from "./App";
-const ProductList = ({title, products}) => {
+import {useEffect, useState} from 'react';
+
+const products = [
+    {name:'Torches', artist:'Foster The People', format:'Vinyl',price:'$40.12',picture:'assets/images/product_11.jpg', id:'11'},
+    {name:'Thursday', artist:'The Weeknd', format:'Vinyl',price:'$49.98',picture:'assets/images/product_12.jpg', id:'12'},
+    {name:'Blonde on Blonde', artist:'Bob Dylan', format:'Vinyl',price:'$58.45',picture:'assets/images/product_13.jpg', id:'13'},
+    {name:'Long Lost', artist:'Lord Huron', format:'Cassette',price:'$12.99',picture:'assets/images/product_14.avif', id:'14'},
+    {name:'A Charlie Brown Christmas', artist:'Vince Guaraldi', format:'Cassette',price:'$16.17',picture:'assets/images/product_15.avif', id:'15'},
+    {name:'Masquerade', artist:'Bananarama', format:'Cassette',price:'$19.00',picture:'assets/images/product_16.avif', id:'16'},
+    {name:'At Last', artist:'Etta James', format:'Merch',price:'$15.49',picture:'assets/images/product_17.avif', id:'17'},
+    {name:'Bloom', artist:'RüFÜS DU SOL', format:'Merch',price:'$47.84',picture:'assets/images/product_18.avif', id:'18'},
+    {name:'Daytona Sand', artist:'Orville Peck', format:'Merch',price:'$51.23',picture:'assets/images/product_19.avif', id:'19'},
+
+]  
+
+
+
+const ProductList = ({title}) => {
+    let [cart, setCart] = useState([])
+    let localCart = localStorage.getItem("cart");
+    
+    const addItem = (item) => {
+      let cartCopy = [...cart];
+
+        cartCopy.push(item);
+    
+    
+        setCart(cartCopy)
+    
+        let string = JSON.stringify(cartCopy);
+        localStorage.setItem("cart", string)
+    }
+    
+
     return ( 
         
         <div class="new_arrivals">
             <div class="container">
                 <div class="row">
-                    <div class="col text-center">
-                        <div class="section_title new_arrivals_title">
-                            <h2>{title}</h2>
-                        </div>
-                    </div>
-
                     <div class="row align-items-center"></div>
                     <div class="col text-center">
                         <div class="row">
                             <div class="col">
                                 <div class="product-grid" data-isotope='{ "itemSelector": ".product-item", "layoutMode": "fitRows" }'>
                                 
-                                    {products.filter(criteria => criteria.format = {title}).map(product => (
+                                    {products.filter(criteria => criteria.format == title).map(product => (
                                         <div class="product-item accessories">
                                             <div class="product product_filter">
                                                 <div class="product_image">
@@ -25,11 +51,11 @@ const ProductList = ({title, products}) => {
                                                 </div>
                                                 <div class="favorite favorite_left"></div>
                                                 <div class="product_info">
-                                                <h6 class="product_name"><a href="#"> {product.name} <br /> {product.artist})</a></h6>
+                                                <h6 class="product_name"><a href="#"> {product.name} <br /> {product.artist}</a></h6>
                                                 <div class="product_price">{product.price}</div>
                                                 </div>
                                             </div>
-                                            <div class="red_button add_to_cart_button"><button onClick={() => cart.add(product.id)}>add to cart</button></div>
+                                            <div class="red_button add_to_cart_button"><button onClick={() => addItem(product.id)}>add to cart</button></div>
                                         </div>
                                     ))}
                                 </div>
